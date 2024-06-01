d5e2759703ca06363bf31f454beb5bfad9dbb3c8
b4rtm <bartek.matusiak12@gmail.com>
2024-06-01 16:06:11 +0200

    created match dto and mapper

---
+++ src/main/java/com/example/volleyballscoreboardbackend/dto/MatchDto.java
@@ -0,0 +1,15 @@
+package com.example.volleyballscoreboardbackend.dto;
+
+import lombok.Data;
+
+@Data
+public class MatchDto {
+
+    private String teamA;
+
+    private String teamB;
+
+    private String date;
+
+    private String status;
+}
