3176dbf1ab2058d9e29b557706ef7e46f55d520f
b4rtm <bartek.matusiak12@gmail.com>
2024-06-11 19:29:17 +0200

    match controller fix

--- src/main/java/com/example/volleyballscoreboardbackend/controller/MatchController.java
+++ src/main/java/com/example/volleyballscoreboardbackend/controller/MatchController.java
@@ -4,7 +4,6 @@ import com.example.volleyballscoreboardbackend.dto.MatchDto;
 import com.example.volleyballscoreboardbackend.dto.ScoreDto;
 import com.example.volleyballscoreboardbackend.model.Match;
 import com.example.volleyballscoreboardbackend.service.MatchService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
@@ -15,8 +14,11 @@ import java.util.List;
 @Controller
 public class MatchController {
 
-    @Autowired
-    private MatchService matchService;
+    private final MatchService matchService;
+
+    public MatchController(MatchService matchService) {
+        this.matchService = matchService;
+    }
 
     @MessageMapping("/addMatch")
     @SendTo("/topic/matches")
@@ -31,23 +33,18 @@ public class MatchController {
         return matchService.getAllMatches();
     }
 
-    @MessageMapping("/getMatch/{matchId}")
-    @SendTo("/topic/matches/{matchId}")
-    public Match sendMatch(@DestinationVariable Long matchId) {
-        return matchService.getMatchById(matchId).orElse(new Match());
-    }
-
     @MessageMapping("/updateScore/{matchId}")
-    @SendTo("/topic/matches/{matchId}")
+    @SendTo("/topic/matches")
     public List<Match> updateScore(@DestinationVariable Long matchId, ScoreDto score) {
         matchService.addScore(matchId, score);
         return matchService.getAllMatches();
     }
 
     @MessageMapping("/deleteMatch/{matchId}")
-    @SendTo("/topic/matches/{matchId}")
-    public void deleteMatch(@DestinationVariable Long matchId) {
+    @SendTo("/topic/matches")
+    public List<Match> deleteMatch(@DestinationVariable Long matchId) {
         matchService.deleteMatch(matchId);
+        return matchService.getAllMatches();
     }
 
 }
